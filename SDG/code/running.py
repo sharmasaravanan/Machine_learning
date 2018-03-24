aeba877a5cf3ff4e0366e66e3234c18707e1c356
sharma <sharmasaravanan.1993@gmail.com>
2018-03-24 10:34:38 +0530

    Updated with regularization

--- SDG/code/running.py
+++ SDG/code/running.py
@@ -1,7 +1,12 @@
 import mnist_loader
 import network
+import network2
 
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+net.SGD(training_data, 10, 10, 3.0, test_data=test_data)
+
+#net2 = network2.Network([784, 30, 10])
+
+#net2.SGD(training_data, 30, 10, 0.1, lmbda=5.0,evaluation_data=validation_data, monitor_evaluation_accuracy=True)
