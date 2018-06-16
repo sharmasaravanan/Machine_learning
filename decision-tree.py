7a51a426d4b2a6cfbe497cab870750b0b36da90d
sharma <sharmasaravanan.1993@gmail.com>
2018-06-16 18:57:45 +0530

    updated with real time scenario

--- decision-tree.py
+++ decision-tree.py
@@ -1,13 +1,17 @@
 from sklearn import tree
 
+import pandas as pd
+
+df=pd.read_csv("./weather.csv")
+
 model=tree.DecisionTreeClassifier()
 
-x=[[180,15,0],[167,42,1],[136,35,1],[174,15,0],[141,28,1]]
+x = df.loc[:,"outlook":"wind"]
 
-y=['man','woman','woman','man','woman']
+y = df.play
 
-model=model.fit(x,y)
+model = model.fit(x,y)
 
-prediction=model.predict([[133,37,1]])
+prediction=model.predict([[2,1,1,1]])
 
 print prediction
