0174c3c0160539741d52388328ac669e40698c1f
sharma <sharmasaravanan.1993@gmail.com>
2018-06-22 09:22:49 +0530

    stage 1 for Deep learning

---
+++ Deep_Learning/stage_1.py
@@ -0,0 +1,91 @@
+from __future__ import print_function
+import numpy as np
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import RMSprop, Adam, SGD
+from keras.utils import np_utils
+
+np.random.seed(1671) 
+
+NB_EPOCH = 200
+BATCH_SIZE = 128
+VERBOSE = 1
+NB_CLASSES = 10 
+
+OPTIMIZER = SGD() 
+N_HIDDEN = 128
+VALIDATION_SPLIT=0.2
+
+DROPOUT = 0.3
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+RESHAPED = 784
+
+X_train = X_train.reshape(60000, RESHAPED)
+X_test = X_test.reshape(10000, RESHAPED)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train /= 255
+X_test /= 255
+
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+Y_train = np_utils.to_categorical(y_train, NB_CLASSES)
+Y_test = np_utils.to_categorical(y_test, NB_CLASSES)
+
+#simple neural network
+model = Sequential()
+model.add(Dense(NB_CLASSES, input_shape=(RESHAPED,)))
+model.add(Activation('softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer=OPTIMIZER, metrics=['accuracy'])
+model.fit(X_train, Y_train, batch_size=BATCH_SIZE, epochs=NB_EPOCH,verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
+
+score = model.evaluate(X_test, Y_test, verbose=VERBOSE)
+print("Test score:", score[0])
+print('Test accuracy:', score[1])
+
+
+#ReLu & Hidden layer
+'''
+model = Sequential()
+model.add(Dense(N_HIDDEN, input_shape=(RESHAPED,)))
+model.add(Activation('relu'))
+model.add(Dense(N_HIDDEN))
+model.add(Activation('relu'))
+model.add(Dense(NB_CLASSES))
+model.add(Activation('softmax'))
+model.summary()
+model.compile(loss='categorical_crossentropy', optimizer=OPTIMIZER, metrics=['accuracy'])
+model.fit(X_train, Y_train, batch_size=BATCH_SIZE, epochs=NB_EPOCH,verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
+
+score = model.evaluate(X_test, Y_test, verbose=VERBOSE)
+print("Test score:", score[0])
+print('Test accuracy:', score[1])
+'''
+
+#Regularization
+'''
+model = Sequential()
+model.add(Dense(N_HIDDEN, input_shape=(RESHAPED,)))
+model.add(Activation('relu'))
+model.add(Dropout(DROPOUT))
+model.add(Dense(N_HIDDEN))
+model.add(Activation('relu'))
+model.add(Dropout(DROPOUT))
+model.add(Dense(NB_CLASSES))
+model.add(Activation('softmax'))
+model.summary()
+model.compile(loss='categorical_crossentropy', optimizer=OPTIMIZER, metrics=['accuracy'])
+model.fit(X_train, Y_train, batch_size=BATCH_SIZE, epochs=NB_EPOCH,verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
+
+score = model.evaluate(X_test, Y_test, verbose=VERBOSE)
+print("Test score:", score[0])
+print('Test accuracy:', score[1])
+'''
+
