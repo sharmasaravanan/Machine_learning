d12fa346e2e36dfcd3840614388088d8f74161a9
sharmasaravanan <sharmasaravanan.1993@gmail.com>
2018-06-30 13:10:08 +0530

    updated with python 3.6

--- Cow_Bull.py
+++ Cow_Bull.py
@@ -1,22 +1,22 @@
 import random as r
 num=str(r.randint(100,999))
-print num   
+print (num)
 j=0
 while 1:
-    user=raw_input("Enter your guess: ")
+    user=input("Enter your guess: ")
     cowbull = [0,0]
     if user=="quit" or user=="q":
-        print "good bye..." 
+        print ("good bye..." )
         break  
     for i in range(len(num)):
         if num[i]==user[i]:
-			cowbull[0]+=1
+        	cowbull[0]+=1
     b=len(set(num)&set(user))
     cowbull[1]=b-cowbull[0]
     j+=1
     if cowbull[0]==3:
-        print "you win after",j,"attempts"
+        print ("you win after",j,"attempts")
         break
     else:
         print("Your guess isn't quite right, try again.")
-    print cowbull
+    print (cowbull)
--- Deep_Learning/stage_2.py
+++ Deep_Learning/stage_2.py
@@ -1,4 +1,4 @@
-from keras import backend as K
+from keras import backend as k
 from keras.models import Sequential
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
@@ -15,7 +15,7 @@ class LeNet:
 	@staticmethod
 	def build(input_shape, classes):
 		model = Sequential()
-		model.add(Convolution2D(20, kernel_size=5, padding="same",input_shape=input_shape))
+		model.add(Conv2D(20, kernel_size=5, padding="same",input_shape=input_shape))
 		model.add(Activation("relu"))
 		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 		
