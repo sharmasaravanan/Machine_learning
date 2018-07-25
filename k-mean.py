5ffaec59ec744f32ce28a6c3627303b19693cfdf
sharmasaravanan <sharmasaravanan.1993@gmail.com>
2018-07-25 07:37:59 +0530

    updated on 25/7/18 from 19/7/18

--- k-mean.py
+++ k-mean.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+style.use("ggplot")
 from sklearn.cluster import KMeans
 
 X = np.array([[1, 2],
--- python-web/web.py
+++ python-web/web.py
@@ -1,16 +1,15 @@
-
 from flask import Flask,render_template,url_for,redirect,request
 
 app=Flask(__name__)
 
-@app.route('/home', methods=['POST', 'GET'])
+@app.route('/display', methods=['POST', 'GET'])
 def hello():
     if request.method == 'GET':
                 return render_template('testing.html')
     elif request.method == 'POST':
-    		na=request.form['img']
-    		kwargs={'name':na,}
-    		return render_template("testing.html",**kwargs)
+		na=request.form['img']
+		kwargs={'name':na,}
+		return render_template("testing.html",**kwargs)
 	
 @app.route('/play')
 def playing():
