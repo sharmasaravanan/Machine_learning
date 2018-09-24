19f64598f32aac6b5592c4a933a0283d2cd168be
sharmasaravanan <sharmasaravanan.1993@gmail.com>
2018-09-24 12:19:13 +0530

    updated on 24/9/18

--- python-web/keyy.py
+++ python-web/keyy.py
@@ -1,20 +1,18 @@
+from flask import Flask,request,jsonify #Django,cherrypy,bottle
 import json
-
-from flask import Flask, request  # Django,cherrypy,bottle
 
 app=Flask(__name__)
 
-
-@app.route('/python', methods=['POST', 'GET'])
+@app.route('/python',methods=['POST','GET'])
 def home():
     if request.method=='POST':
-        # data=request.args.get('d')#request.args['q']
-        data = request.values.get('fruit')
+             #data=request.args.get('d')#request.args['q']
+             data = request.values.get('fruit')
              print(data)
              res='{'+'"Name":"{}"'.format(data)+'}'
-        print(json.loads(res))
-        #return jsonify(res)
+             print(json.loads(res))
+             #return jsonify(res)
              return json.dumps(json.loads(res))
              
 if __name__=='__main__':
-    app.run(host="0.0.0.0", port=8000)
+	app.run(host="0.0.0.0",port=8000)
