814642992745207178dd7be1b8d00335b50c5b8a
sharmasaravanan <sharmasaravanan.1993@gmail.com>
2018-11-10 21:39:19 +0530

    updated on 10/10/18
    Signed-off-by: sharma-saravanan <sharma@agentz.ai>

---
+++ correl.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv("./Advertising.csv", index_col=0)
+corr = data.corr()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(corr, cmap='coolwarm', vmin=-1, vmax=1)
+fig.colorbar(cax)
+ticks = np.arange(0, len(data.columns), 1)
+ax.set_xticks(ticks)
+plt.xticks(rotation=90)
+ax.set_yticks(ticks)
+ax.set_xticklabels(data.columns)
+ax.set_yticklabels(data.columns)
+plt.show()
