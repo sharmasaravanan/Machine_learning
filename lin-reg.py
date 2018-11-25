60b09e4366ed79c4db35045ee7f8f568adfdced8
sharma <sharmasaravanan.1993@gmail.com>
2018-11-25 16:56:51 +0530

    updated on 25/11/18
    Signed-off-by: sharma-saravanan <sharma@agentz.ai>

--- lin-reg.py
+++ lin-reg.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn import metrics
-from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 # import pickle
 
