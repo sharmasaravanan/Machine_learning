ce6c71937c3f77c6cbdf9d55b30e330e06a472b9
sharma-saravanan <sharma@agentz.ai>
2019-03-24 19:39:07 +0530

    updated on 24/03/19
    Signed-off-by: sharma-saravanan <sharma@agentz.ai>

--- SDG/code/running.py
+++ SDG/code/running.py
@@ -4,7 +4,7 @@ import network
 
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net = network.Network([784, 30, 30, 30, 10])
+net = network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
 #net2 = network2.Network([784, 30,30,30, 10])
