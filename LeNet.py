0fe4137b43ee214cb2dec4fd1368500ac7bb4e2e
sharma-saravanan <sharma@agentz.ai>
2019-04-27 19:32:06 +0530

    updated on 27/04/19
    Signed-off-by: sharma-saravanan <sharma@agentz.ai>

---
+++ LeNet.py
@@ -0,0 +1,41 @@
+# import the necessary packages
+from keras import backend as K
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Dense
+from keras.layers.core import Flatten
+from keras.models import Sequential
+
+
+class LeNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model
+        model = Sequential()
+        inputShape = (height, width, depth)
+
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+
+        # first set of CONV => RELU => POOL layers
+        model.add(Conv2D(20, (5, 5), padding="same", input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+        # second set of CONV => RELU => POOL layers
+        model.add(Conv2D(50, (5, 5), padding="same"))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+        # first set of FC => RELU layers
+        model.add(Flatten())
+        model.add(Dense(500))
+        model.add(Activation("relu"))
+
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        # return the constructed network architecture
+        return model
