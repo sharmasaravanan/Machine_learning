4e107eb3a68d58b73461d2ed05480d8a1c951f75
sharma-saravanan <sharma@agentz.ai>
2019-06-30 20:32:23 +0530

    updated on 30/06/19
    Signed-off-by: sharma-saravanan <sharma@agentz.ai>

--- Deep_Learning/CNN_cifar.py
+++ Deep_Learning/CNN_cifar.py
@@ -1,67 +1,88 @@
-from keras.datasets import cifar10
-from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.models import Sequential
-from keras.optimizers import Adam
-from keras.utils import np_utils
+import numpy  
+from keras.models import Sequential  
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation  
+from keras.layers.convolutional import Conv2D, MaxPooling2D  
+from keras.constraints import maxnorm  
+from keras.utils import np_utils  
+from keras.datasets import cifar10  
 
-# CIFAR_10 is a set of 60K images 32x32 pixels on 3 channels
-IMG_CHANNELS = 3
-IMG_ROWS = 32
-IMG_COLS = 32
 
 #constant
-BATCH_SIZE = 128
-NB_EPOCH = 20
-NB_CLASSES = 10
-VERBOSE = 1
-VALIDATION_SPLIT = 0.1
-OPTIM = Adam()
+epochs = 25  
+optimizer = 'adam'  
 
-#load dataset
+# loading in the data
+# CIFAR_10 is a set of 60K images 32x32 pixels on 3 channels
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
 print('X_train shape:', X_train.shape)
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
-# convert to categorical
-Y_train = np_utils.to_categorical(y_train, NB_CLASSES)
-Y_test = np_utils.to_categorical(y_test, NB_CLASSES)
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)  
+y_test = np_utils.to_categorical(y_test)  
+class_num = y_test.shape[1]
 
-# float and normalization
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
+# normalize the inputs from 0-255 to between 0 and 1 by dividing by 255
+
+X_train = X_train.astype('float32')  
+X_test = X_test.astype('float32')  
+X_train = X_train / 255.0  
+X_test = X_test / 255.0  
 
 # network
-model = Sequential()
-model.add(Conv2D(32, kernel_size=5, padding='same',input_shape=(IMG_ROWS, IMG_COLS, IMG_CHANNELS)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Conv2D(50, kernel_size=5, border_mode="same"))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(NB_CLASSES))
-model.add(Activation('softmax'))
+model = Sequential()  
+model.add(Conv2D(32, (3, 3), input_shape=X_train.shape[1:], padding='same'))  
+model.add(Activation('relu'))  
+#model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), activation='relu', padding='same'))  
+model.add(Dropout(0.2))  
+model.add(BatchNormalization())  
 
+model.add(Conv2D(64, (3, 3), padding='same'))  
+model.add(Activation('relu')
+model.add(MaxPooling2D(pool_size=(2, 2)))  
+model.add(Dropout(0.2))  
+model.add(BatchNormalization()) 
+
+model.add(Conv2D(64, (3, 3), padding='same'))  
+model.add(Activation('relu'))  
+model.add(MaxPooling2D(pool_size=(2, 2)))  
+model.add(Dropout(0.2))  
+model.add(BatchNormalization())
+
+model.add(Conv2D(128, (3, 3), padding='same'))  
+model.add(Activation('relu'))  
+model.add(Dropout(0.2))  
+model.add(BatchNormalization()) 
+
+model.add(Flatten())  
+model.add(Dropout(0.2))  
+
+model.add(Dense(256, kernel_constraint=maxnorm(3)))  
+model.add(Activation('relu'))  
+model.add(Dropout(0.2))  
+model.add(BatchNormalization())
+
+model.add(Dense(128, kernel_constraint=maxnorm(3)))  
+model.add(Activation('relu'))  
+model.add(Dropout(0.2))  
+model.add(BatchNormalization()) 
+
+model.add(Dense(class_num))  
+model.add(Activation('softmax'))
+model.summary()
 
 
 # train
-model.compile(loss='categorical_crossentropy', optimizer=OPTIM,metrics=['accuracy'])
-model.fit(X_train, Y_train, batch_size=BATCH_SIZE,epochs=NB_EPOCH, validation_split=VALIDATION_SPLIT,verbose=VERBOSE)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])  
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=64)  
 
-score = model.evaluate(X_test, Y_test,batch_size=BATCH_SIZE, verbose=VERBOSE)
 
-# print("Test score:", score[0])
-print('Test accuracy:', score[1])
-model.summary()
+# Model evaluation
+scores = model.evaluate(X_test, y_test, verbose=0)  
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
 #save model
 model_json = model.to_json()
 open('cifar10_architecture.json', 'w').write(model_json)
