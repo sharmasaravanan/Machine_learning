beeb61e90b50b758be32a1177d1f1baf4bd1e542
sharma-saravanan <sharma@agentz.ai>
2019-10-06 11:56:50 +0530

    Updated on 06/10/19
    Signed-off-by: sharma-saravanan <sharma@agentz.ai>

--- python-web/keyy.py
+++ python-web/keyy.py
@@ -15,4 +15,4 @@ def home():
 
              
 if __name__=='__main__':
-	app.run(host="0.0.0.0",port=8000)
+    app.run()
--- python-web/web.py
+++ python-web/web.py
@@ -22,4 +22,4 @@ def playing():
 	return redirect(url_for('test'))
 	
 if __name__=='__main__':
-    app.run()
+    app.run(port=8005)
