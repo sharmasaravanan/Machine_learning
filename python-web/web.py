239261636e4be75d3c715969a1f9c9eba3580d52
sharma-saravanan <sharma@agentz.ai>
2019-11-02 16:00:23 +0530

    Updated on 02/11/19
    Signed-off-by: sharma-saravanan <sharma@agentz.ai>

--- python-web/web.py
+++ python-web/web.py
@@ -1,5 +1,9 @@
+import MySQLdb
+from flask import Flask, render_template, url_for, redirect, request
 
-from flask import Flask,render_template,url_for,redirect,request
+db = MySQLdb.connect(host="localhost", user="admin", passwd="test@123", db="testDB")
+
+cur = db.cursor()
 
 app=Flask(__name__)
 
@@ -8,9 +12,11 @@ def hello():
     if request.method == 'GET':
                 return render_template('testing.html')
     elif request.method == 'POST':
-    		na=request.form['img']
-    		kwargs={'name':na,}
-    		return render_template("testing.html",**kwargs)
+        na = request.form['img']
+        cur.execute("SELECT Passport_Number FROM testDB.PassportDetails where Person_Id = {}".format(na))
+        name = cur.fetchone()[0]
+        kwargs = {'name': name, }
+        return render_template("testing.html", **kwargs)
 
 @app.route('/testing',methods=['GET'])
 def test():
@@ -22,4 +28,4 @@ def playing():
 	return redirect(url_for('test'))
 	
 if __name__=='__main__':
-    app.run(port=8005)
+    app.run(host="0.0.0.0", port=8005)
