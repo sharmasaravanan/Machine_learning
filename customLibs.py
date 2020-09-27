657b1942bb72682f420e89721dc1ceb5143a97f7
sharma-saravanan <sharma@agentz.ai>
2020-09-27 20:36:58 +0530

    Updated on 27/09/20
    Signed-off-by: sharma-saravanan <sharma@agentz.ai>

--- customLibs.py
+++ customLibs.py
@@ -1,2 +1,7 @@
 def hello():
     print("I am from custom Lib")
+
+    
+class A:
+    def display(self):
+        print("I am from class A")
--- testing.py
+++ testing.py
@@ -1,3 +1,9 @@
 def sumALL(l):
     ans = sum(l)
     return ans
+from customLibs import hello,A
+
+hello()
+
+a = A()
+a.display()
