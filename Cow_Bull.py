7ca0333405732c70fcd57b395a1a58845552d8dc
sharmasaravanan <Secure@1109>
2021-07-03 11:42:26 +0530

    updated on 2021/07/03

--- Cow_Bull.py
+++ Cow_Bull.py
@@ -1,22 +1,26 @@
 import random as r
-num=str(r.randint(100,999))
-print (num)
-j=0
+num = str(r.randint(100, 999))
+print(num)
+
+j = 0
 while 1:
-    user=input("Enter your guess: ")
-    cowbull = [0,0]
-    if user=="quit" or user=="q":
-        print ("good bye..." )
-        break  
+    user = eval(input("Enter your guess: "))
+    cowbull = [0, 0]
+    if user == "quit" or user == "q":
+        print("good bye...")
+        break
+        
     for i in range(len(num)):
-        if num[i]==user[i]:
-        	cowbull[0]+=1
-    b=len(set(num)&set(user))
-    cowbull[1]=b-cowbull[0]
-    j+=1
-    if cowbull[0]==3:
-        print ("you win after",j,"attempts")
+        if num[i] == user[i]:
+            cowbull[0] += 1
+
+    common = len([i for i in comp if i in user])
+    cowbull[1] = common - cowbull[0]
+    j += 1
+
+    if cowbull[0] == 3:
+        print(("you win after", j, "attempts"))
         break
     else:
         print("Your guess isn't quite right, try again.")
-    print("Cow : %d and Bull : %d" % (cowbull[0], cowbull[1]))
+    print(("Cow : %d and Bull : %d" % (cowbull[0], cowbull[1])))
--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,14 +1,15 @@
 j=["rock","scissor","paper"]
 while 1:
-    user = input("Enter your choice :")
+    user = input("Enter your player-1 :")
     if user == "quit" or user == "q":
         print("Good Bye....")
         break
-    comp = input("Enter your choice :")  # r.choice(j)
+        
+    comp = input("Enter your player-2 :")
     if comp == "quit" or comp == "q":
         print("Good Bye....")
         break
-    print("Your choice is ", user, "and computer choice is ", comp)
+    print("player_1 choice is ", user, "and player_2 choice is ", comp)
     if user == comp:
         print("Game is tie")
         continue
